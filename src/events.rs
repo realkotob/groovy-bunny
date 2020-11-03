ecad5e091e79654334a4909160c97ec0c10b8afd
asheraryam <asheraryam@gmail.com>
2020-11-03 17:40:16 +0300

    feat: Add reminders for QA day

--- src/events.rs
+++ src/events.rs
@@ -2,12 +2,16 @@
 use super::parse_time;
 use super::storage;
 use chrono::Utc;
+use serenity::model::id::{ChannelId, MessageId};
 use std::io::{BufRead, BufReader, Error, Read, Write};
+extern crate job_scheduler;
+use job_scheduler::{Job, JobScheduler};
+use std::time::Duration;
 
 use serenity::{
     model::{
         channel::{Message, Reaction, ReactionType},
-        gateway::Ready,
+        gateway::{Activity, Ready},
     },
     prelude::{Context, EventHandler},
 };
@@ -137,21 +141,33 @@ impl EventHandler for Handler {
     fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is ready", ready.user.name);
 
-        use serenity::model::gateway::Activity;
         ctx.set_activity(Activity::playing(&String::from(
             "Oh dear! I shall be too late!",
         )));
 
-        // schedule_worklog(&ctx);
-        extern crate job_scheduler;
-        use job_scheduler::{Job, JobScheduler};
-        use std::time::Duration;
         let mut sched = JobScheduler::new();
+
         sched.add(Job::new("0 0 9 * * FRI".parse().unwrap(), || {
-            println!("check_work_log...");
+            println!("check_work_log ...");
             match check_work_log(&ctx) {
                 Ok(x) => println!("Checked worklog loaded."),
                 Err(why) => println!("Error checking worklog {:?}", why),
+            };
+        }));
+
+        sched.add(Job::new("0 0 15 * * TUE".parse().unwrap(), || {
+            println!("send_qa_day_dev_reminder ...");
+            match send_qa_day_dev_reminder(&ctx) {
+                Ok(x) => println!("Sent QA day dev reminder."),
+                Err(why) => println!("Error sending QA day dev reminder {:?}", why),
+            };
+        }));
+
+        sched.add(Job::new("0 0 9 * * WED".parse().unwrap(), || {
+            println!("send_qa_day_all_reminder ...");
+            match send_qa_day_all_reminder(&ctx) {
+                Ok(x) => println!("Sent QA dev reminder."),
+                Err(why) => println!("Error sending QA day reminder {:?}", why),
             };
         }));
 
@@ -168,14 +184,65 @@ impl EventHandler for Handler {
     }
 }
 
-fn schedule_worklog(ctx: &Context) {}
+fn send_qa_day_dev_reminder(ctx: &Context) -> Result<(), Error> {
+    let dev_reminder_channel_id: u64 = 705037778471223339;
+
+    let dev_reminder_chan = ctx.http.get_channel(dev_reminder_channel_id);
+    let dev_role_id: u64 = 705034249652273153;
+
+    let msg_dev_remider = format!(
+        "Tomorrow is QA Day! Get your test builds ready <@&{}> !",
+        dev_role_id.to_string()
+    );
+
+    match dev_reminder_chan {
+        Ok(x) => {
+            match x.guild() {
+                Some(guild_lock) => {
+                    guild_lock.read().say(&ctx.http, msg_dev_remider);
+                }
+                None => {
+                    println!("It's not a guild!");
+                }
+            };
+        }
+        Err(why) => {
+            println!("Error sending message to dev reminder channel. {:?}", why);
+        }
+    };
+
+    Ok(())
+}
+
+fn send_qa_day_all_reminder(ctx: &Context) -> Result<(), Error> {
+    let dev_reminder_channel_id: u64 = 705090277794119790;
+
+    let dev_reminder_chan = ctx.http.get_channel(dev_reminder_channel_id);
+
+    let msg_dev_remider = format!("Today is QA Day! Happy testing @here !",);
+
+    match dev_reminder_chan {
+        Ok(x) => {
+            match x.guild() {
+                Some(guild_lock) => {
+                    guild_lock.read().say(&ctx.http, msg_dev_remider);
+                }
+                None => {
+                    println!("It's not a guild!");
+                }
+            };
+        }
+        Err(why) => {
+            println!("Error sending message to dev reminder channel. {:?}", why);
+        }
+    };
+
+    Ok(())
+}
 
 fn check_work_log(ctx: &Context) -> Result<(), Error> {
-    use serenity::model::gateway::Activity;
-    use serenity::model::id::{ChannelId, MessageId};
-
-    let worklog_channel_id = 705067423530745957;
-    // let test_channel_id = 770656917415788545;
+    let worklog_channel_id: u64 = 705067423530745957;
+    // let test_channel_id: u64 = 770656917415788545;
 
     let channel_id = ChannelId(worklog_channel_id);
 
